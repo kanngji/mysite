ecc1bf724e5427e7f6590a4c87a6d99e30549741
kanngji <alleneffort1994@gmail.com>
2022-12-20 11:16:04 +0900

    URL 매핑과 css 파일 추가 (답변)

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
 from . import views
+
+app_name = 'pybo'
 urlpatterns = [
     # ''쓰는 이유 => config/urls.py 파일에서 이미
     # pybo/ 로 시작하는 URL이 pybo/urls.py 파일과 먼저 매핑
 
-    path('', views.index),
-    path('<int:question_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
